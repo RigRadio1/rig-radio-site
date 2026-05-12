7e1dc18c68493d9a67c575b0e0048f84b2bb4016
Mike <info@tex1star.com>
2026-05-12 12:29:23 -0500

    Add trending genre scroll arrows

--- assets/members/home.js
+++ assets/members/home.js
@@ -159,6 +159,15 @@
       openGenre(btn.dataset.genre);
     });
 
+    
+    document.getElementById("genreScrollLeft")?.addEventListener("click", () => {
+      tilesEl.scrollBy({ left: -360, behavior: "smooth" });
+    });
+
+    document.getElementById("genreScrollRight")?.addEventListener("click", () => {
+      tilesEl.scrollBy({ left: 360, behavior: "smooth" });
+    });
+
     openGenre(genres[0]);
   };
 
